e911c64ebc2ae1245cc03c41c0ac40fdaec781c7
Andrew C. Greenberg <wizardwerdna@gmail.com>
2014-04-07 15:31:53 -0700

    Change to first scenario, requiring that list be sorted by date

--- test/features.js
+++ test/features.js
@@ -1,20 +1,33 @@
 'use strict';
 describe('Superheroic Timekeeping', function(){
-  var page, entries;
-  var entry1 = {date: '2014-01-01', desc: 'Description1', hour: 1.1};
-  var entry2 = {date: '2014-01-02', desc: 'Description2', hour: 1.2};
+  var page;
+  var entry1 = {date: '2014-01-01', desc: 'Description1', hour: 1.1},
+      entry2 = {date: '2014-01-02', desc: 'Description2', hour: 1.2},
+      entry3 = {date: '2014-01-03', desc: 'Description3', hour: 1.3};
 
-  describe('FEATURE: Display list of timesheet entries.', function(){
+  describe('FEATURE: Display list of timesheet entries, sorted by date.',
+    function(){
     it('SCENARIO: There are no timesheet entries', function(){
       GIVEN_TimesheetEntriesFrom([]);
       THEN_TimesheetEntryDisplayConformsTo([]);
     });
 
     it('SCENARIO: There are some timesheet entries.', function(){
-      GIVEN_TimesheetEntriesFrom([entry1, entry2]);
-      THEN_TimesheetEntryDisplayConformsTo([entry1, entry2]);
+      GIVEN_TimesheetEntriesFrom([entry3, entry1, entry2]);
+      THEN_TimesheetEntryDisplayConformsTo([entry1, entry2, entry3]);
     });
   });
+
+  describe('FEATURE: Create a new timesheet entry.', function(){
+    it('SCENARIO: Fill out a new form and submit.', function(){
+    });
+    it('SCENARIO: Fill out a new form and cancel.', function(){
+    });
+  });
+
+  describe('FEATURE: Update an existing timesheet entry.', function(){ });
+
+  describe('FEATURE: Destroy an existing timesheet entry.', function(){ });
 
   function GIVEN_TimesheetEntriesFrom(entries){
     module('app', 'index.html');
@@ -42,4 +55,5 @@ describe('Superheroic Timekeeping', function(){
       };
     })).toEqual(entries);
   }
+
 });
